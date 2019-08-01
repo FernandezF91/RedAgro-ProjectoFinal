e86913af791dcea3e7918d0a6340d9ea40205cce
ccorbo <corbocecilia@gmail.com>
2019-07-31 21:25:55 -0300

    recuperar email

--- frontend/src/Recuperaremail.js
+++ frontend/src/Recuperaremail.js
@@ -1,6 +1,13 @@
+//este import para cabecera
 import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar';
 import culturaVerde from './cultura-verde.png';
+
+//este import para cuerpo   
+import './recuperaremail.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
 
 class Recuperaremail extends Component {
 
@@ -13,6 +20,22 @@ class Recuperaremail extends Component {
 						<img src={culturaVerde} width="150px" height="60px"></img>
 					</Navbar>
 				</div>
+				<Container fluid className="contenedorrecu">
+					<div className="formularioRecuContra">
+						<h2>Recuperar Contraseña</h2>
+					</div>
+					<div className="encabezadoRecucontra">
+						<Form>
+							<div className="Correoelectronico">
+									<Form.Group as={Row} controlId="formHorizontalEmail">
+										<Form.Label>
+											Correo electronico:
+                                		</Form.Label>
+									</Form.Group>
+							</div>
+						</Form>
+					</div>
+				</Container>
 			</body>
 				);
 
