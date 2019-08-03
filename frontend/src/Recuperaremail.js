090d649418ad51427400b9221e50926a7d10c121
ccorbo <corbocecilia@gmail.com>
2019-08-03 14:01:59 -0300

    pantalla recuperaemail

--- frontend/src/Recuperaremail.js
+++ frontend/src/Recuperaremail.js
@@ -8,10 +8,55 @@ import './recuperaremail.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+
+//botones y comentario
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 
 class Recuperaremail extends Component {
 
-		render() {
+	constructor(props) {
+
+		super(props)
+
+		this.state = {
+			fields: [],
+			errores: []
+		}
+
+		this.validarDatos = this.validarDatos.bind(this);
+	}
+
+	detectarCambios(e) {
+
+		let fields = this.state.fields;
+		fields[e.target.name] = e.target.value;
+		this.setState({
+			fields
+		})
+
+	}
+
+	validarDatos() {
+
+		this.setState({
+			errores: []
+		})
+
+		let errores = {};
+
+		if (!this.state.fields["emailuser"]) {
+
+			errores["emailuser"] = "*Completar campo";
+		} 
+
+		this.setState({
+			errores
+		})
+	}
+
+	render() {
 
 		return (
 			<body>
@@ -23,7 +68,6 @@ class Recuperaremail extends Component {
 				<Container fluid className="contenedorrecu">
 					<div className="formularioRecuContra">
 						<h2>Recuperar Contraseña</h2>
-					</div>
 					<div className="encabezadoRecucontra">
 						<Form>
 							<div className="Correoelectronico">
@@ -31,9 +75,18 @@ class Recuperaremail extends Component {
 										<Form.Label>
 											Correo electronico:
                                 		</Form.Label>
+										<Col>
+											<Form.Control type="recuemail" name="emailuser" onChange={(e) => this.detectarCambios(e)} />
+											<div className="error">{this.state.errores["emailuser"]}</div>
+										</Col>
 									</Form.Group>
 							</div>
 						</Form>
+					</div>
+					</div>
+					<div className="botonesEmail">
+						<Nav.Link className="botoncanrecup" href='/login'><Button variant="success">Cancelar</Button></Nav.Link>
+						<Nav.Link className="botonconfrecu" href=''><Button variant="success" onClick={this.validarDatos}>Confirmar</Button></Nav.Link>
 					</div>
 				</Container>
 			</body>
