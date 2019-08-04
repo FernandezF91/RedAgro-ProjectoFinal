0844b0fcb03daddd2b8650b11d043565cf06d04b
ccorbo <corbocecilia@gmail.com>
2019-08-04 16:28:39 -0300

    avances pantalla principal consumidores

--- frontend/src/PantallaPrincipalConsumidores.js
+++ frontend/src/PantallaPrincipalConsumidores.js
@@ -3,6 +3,12 @@ import './PantallaPrincipalConsumidores.css';
 import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar';
 import culturaVerde from './cultura-verde.png';
+import Micuenta from './Micuenta.png';
+import campanalertas from './campanalertas.png';
+import configuracion from './configuracion.png';
+import reservas from './reservas.png';
+import compras from './compras.png';
+import preferencias from './preferencias.png';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import { Dropdown } from 'react-bootstrap';
@@ -40,17 +46,17 @@ class PantallaPrincipalconsumidores extends Component {
 					<div className="menuConsumidor">
 					<nav>
        	 				<ul>
-            				<li><a href="#">Mi cuenta</a></li>
-							<li><a href="#">Reservas</a></li>
-							<li><a href="#">Alertas</a></li>
-							<li><a href="#">Preferencias</a></li>
-            				<li><a href="#">Comprar</a>
+            				<li><a href="#"><img src={Micuenta} width="30px" height="25px"></img> Mi cuenta</a></li>
+							<li><a href="#"><img src={reservas} width="30px" height="25px"></img> Reservas</a></li>
+							<li><a href="#"><img src={campanalertas} width="30px" height="30px"></img> Alertas</a></li>
+							<li><a href="#"><img src={preferencias} width="30px" height="19px"></img> Preferencias</a></li>
+            				<li><a href="#"><img src={compras} width="30px" height="19px"></img> Comprar</a>
 								<ul>
                 					<li><a href="#">Geolocalizacion</a></li>
                 					<li><a href="#">Categorias</a></li>
             					</ul>
 							</li>
-            				<li><a href="#">Configuracion</a>
+            				<li><a href="#"><img src={configuracion} width="30px" height="25px"></img> Configuracion</a>
 								<ul>
                 					<li><a href="/login">editar datos usuario</a></li>
                 					<li><a href="/login">modificar contraseña</a></li>
