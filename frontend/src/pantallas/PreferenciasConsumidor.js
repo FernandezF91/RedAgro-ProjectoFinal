1572ad68eba8f1361b5e4c5a582feb7330dca58c
Vanesa Molina <vmolina@grupoassa.com>
2019-09-09 13:08:17 -0300

    Update Preferencias
    Carga de dropdown con la base

--- frontend/src/pantallas/PreferenciasConsumidor.js
+++ frontend/src/pantallas/PreferenciasConsumidor.js
@@ -8,62 +8,138 @@ import Button from 'react-bootstrap/Button';
 //https://alligator.io/react/react-select/
 //Por si les hace falta: npm install react-select@next
 
-const frutas = [
-	{ label: "Manzana", value: 1 },
-	{ label: "Naranja", value: 2 },
-	{ label: "Pomelo blanco", value: 3 },
-	{ label: "Pomelo rosado", value: 4 },
-	{ label: "Sandia", value: 5 },
-	{ label: "Melon", value: 6 },
-];
-
-const verduras = [
-	{ label: "Tomate perita", value: 1 },
-	{ label: "Tomate cherry", value: 2 },
-	{ label: "Pepino", value: 3 },
-	{ label: "Lechuga", value: 4 },
-	{ label: "Repollo", value: 5 },
-	{ label: "Zapallo", value: 6 },
-];
-
-const otros = [
-	{ label: "Aceitunas negras", value: 1 },
-	{ label: "Aceituas verdes", value: 2 },
-	{ label: "Miel", value: 3 },
-	{ label: "Pickles", value: 4 },
-];
-
-
 class PreferenciasConsumidor extends Component {
 
 	constructor(props) {
 		super(props)
-		
+
 		this.state = {
 			active: [],
 			id: this.props.id_consumidor,
-		}
+			preferencias: [],
+			seleccionados: {
+				verduras: [],
+				frutas: [],
+				otros: [],
+			},
+			otros: [],
+			verduras: [],
+			frutas: []
+		};
 
 		this.mostrarPantallaPrincipal = this.mostrarPantallaPrincipal.bind(this);
 	}
 
 	mostrarPantallaPrincipal() {
-
 		this.props.history.push({
 			pathname: '/principalConsumidores',
 			state: { id: this.state.id }
 		})
-
 	}
-	
+
 	onChange(value, key) {
 		this.setState({
 			[key]: value
 		})
 	}
 
-	render() {
+	addNewPreferencia(categoria, newPreferencia) {
+		if (categoria === "verduras") {
+			this.setState({ seleccionados: {...this.state.seleccionados, verduras: newPreferencia } });
+		}
+		if (categoria === "frutas") {
+			this.setState({ seleccionados: { ...this.state.seleccionados, frutas: newPreferencia } });
+		}
+		if (categoria === "otros") {
+			this.setState({ seleccionados: { ...this.state.seleccionados, frutas: newPreferencia } });
+		}
+	}
 
+	getPreferencesSaved(data, categoria) {
+		var itemList = [];
+		var dataFiltered = data.filter(function (item) {
+			return item.producto.categoria === categoria
+		}
+		).map(function (item) {
+			return {
+				label: item.producto.tipo,
+				value: true
+			}
+		});
+
+		if (dataFiltered.length > 0) {
+			itemList = dataFiltered;
+		}
+		return itemList;
+	}
+
+	componentDidMount() {
+		//Obtengo las preferencias del Consumidor
+		var path = "http://localhost:3000/redAgro/preferencia_consumidor?id=" + this.state.id;
+		fetch(path)
+			.catch(err => console.error(err))
+			.then(response => { return response.json(); })
+			.then(data => {
+				this.setState({
+					seleccionados: {
+						verduras: this.getPreferencesSaved(data, "Verduras"),
+						frutas: this.getPreferencesSaved(data, "Frutas"),
+						otros: this.getPreferencesSaved(data, "Otros")
+					},
+					preferencias: data.map((item) => {
+						return {
+							categoria: item.producto.categoria,
+							tipo: item.producto.tipo,
+							id: item.producto.id
+						}
+					})
+				});
+			})
+		//Cargo los productos 'Frutas'
+		fetch('http://localhost:3000/redAgro/obtener_tipo_productos?categoria=frutas')
+			.catch(err => console.error(err))
+			.then(response => { return response.json(); })
+			.then(data => {
+				this.setState({
+					frutas: data.map((item) => {
+						return {
+							label: item.tipo,
+							value: item.id
+						}
+					})
+				});
+			})
+		//Cargo los productos 'verduras'
+		fetch('http://localhost:3000/redAgro/obtener_tipo_productos?categoria=verduras')
+			.catch(err => console.error(err))
+			.then(response => { return response.json(); })
+			.then(data => {
+				this.setState({
+					verduras: data.map((item) => {
+						return {
+							label: item.tipo,
+							value: item.id
+						}
+					})
+				});
+			})
+		//Cargo los productos 'Otros'
+		fetch('http://localhost:3000/redAgro/obtener_tipo_productos?categoria=otros')
+			.catch(err => console.error(err))
+			.then(response => { return response.json(); })
+			.then(data => {
+				this.setState({
+					otros: data.map((item) => {
+						return {
+							label: item.tipo,
+							value: item.id
+						}
+					})
+				});
+			})
+	}
+
+	render() {
 		return (
 			<div className="container">
 				<div className="titulosPrincipales">Preferencias</div>
@@ -71,20 +147,43 @@ class PreferenciasConsumidor extends Component {
 					<h5>Seleccione sus productos de interés para recibir novedades sobre los mismos:</h5>
 				</div>
 				<br />
+
 				<div className="opciones">
 					<div className="tituloProductos">Verduras:</div>
-					<Select className="dropdownProductos" options={verduras} placeholder="Seleccione uno o varios items..." isMulti onChange={opt => console.log(opt.label, opt.value)} />
+					<Select className="dropdownProductos"
+						value={this.state.seleccionados.verduras}
+						options={this.state.verduras}
+						placeholder="Seleccione uno o varios items..."
+						isMulti
+						//newPreferencia.label, newPreferencia.value)} />
+						onChange={newVerdura => this.addNewPreferencia("verduras", newVerdura)} />
 				</div>
 				<br />
+
 				<div className="opciones">
 					<div className="tituloProductos">Frutas:</div>
-					<Select className="dropdownProductos" options={frutas} placeholder="Seleccione uno o varios items..." isMulti onChange={opt => console.log(opt.label, opt.value)} />
+					<Select className="dropdownProductos"
+						//	defaultValue={this.state.frutasSelect}
+						value={this.state.seleccionados.frutas}
+						options={this.state.frutas}
+						placeholder="Seleccione uno o varios items..."
+						isMulti
+						//onChange={opt => console.log(opt.label, opt.value)} />
+						onChange={newFruta => this.addNewPreferencia("frutas", newFruta)} />
 				</div>
 				<br />
+
 				<div className="opciones">
 					<div className="tituloProductos">Otros:</div>
-					<Select className="dropdownProductos" options={otros} placeholder="Seleccione uno o varios items..." isMulti onChange={opt => console.log(opt.label, opt.value)} />
+					<Select className="dropdownProductos"
+						value={this.state.seleccionados.otros}
+						options={this.state.otros}
+						placeholder="Seleccione uno o varios items..."
+						isMulti
+						//	onChange={opt => console.log(opt.label, opt.value)} />
+						onChange={newOtros => this.addNewPreferencia("otros", newOtros)} />
 				</div>
+
 				<br />
 				<div className="botonesPreferencias">
 					<div className="botonCrear">
@@ -98,5 +197,4 @@ class PreferenciasConsumidor extends Component {
 		)
 	};
 }
-
 export default PreferenciasConsumidor;
