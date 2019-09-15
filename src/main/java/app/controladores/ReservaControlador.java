61874f29b770224778fa4c2bf7103ebb2577452a
barbsabakdanian <50269124+barbsabakdanian@users.noreply.github.com>
2019-09-15 02:03:21 -0300

    Back estadistica de estatos

--- src/main/java/app/controladores/ReservaControlador.java
+++ src/main/java/app/controladores/ReservaControlador.java
@@ -2,6 +2,11 @@ package app.controladores;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import java.math.BigInteger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import javax.persistence.Query;
 
 import app.clases.Reserva;
@@ -29,23 +37,37 @@ public class ReservaControlador {
 
 	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping(path = "redAgro/get_reservas_usuario")
-
 	@ResponseBody
 	public List<Reserva> obtenerReservasByUsuario(@RequestParam Long id) {
 
 		String tipoUsuario = usuarioDAO.obtenerTipoUsuario(id);
 		ReservaMapper mapeoReservas = new ReservaMapper();
 		List<EntidadReserva> resultados = new ArrayList<>();
-		
-		if(tipoUsuario.equals("Consumidor")) {
+
+		if (tipoUsuario.equals("Consumidor")) {
 			resultados = reservaDao.obtenerReservasByConsumidor(id);
-		}
-		else {
-			if(tipoUsuario.equals("Productor")) {
+		} else {
+			if (tipoUsuario.equals("Productor")) {
 				resultados = reservaDao.obtenerReservasByProductor(id);
 			}
 		}
 		List<Reserva> reservas = mapeoReservas.mapFromEntity(resultados);
 		return reservas;
+	}
+
+	// Solo contabiliza los ultimos 3 meses
+	@CrossOrigin(origins = "http://localhost:3000")
+	@RequestMapping(value = "redAgro/obtenerMetricasReservasPorEstado", method = RequestMethod.GET)
+	public Map<String, BigInteger> obtenerMetricasReservasPorEstado(@RequestParam Long id_usuario) {
+		List<Object[]> resultados = reservaDao.obtenerMetricasReservasPorEstado(id_usuario);
+		
+		Map<String, BigInteger> map = null;
+		if (resultados != null && !resultados.isEmpty()) {
+			map = new HashMap<String, BigInteger>();
+			for (Object[] object : resultados) {
+				map.put(((String) object[0]), ((BigInteger)object[1]));
+			}
+		}
+		return map;
 	}
 }
--- src/main/java/app/daos/ReservaDao.java
+++ src/main/java/app/daos/ReservaDao.java
@@ -2,15 +2,22 @@ package app.daos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
+
 import app.modelos.EntidadReserva;
 
 public interface ReservaDao extends JpaRepository<EntidadReserva, Long> {
 
-	@Query(value="SELECT * FROM Reserva WHERE consumidor_id = ?1", nativeQuery = true)
+	@Query(value = "SELECT * FROM Reserva WHERE consumidor_id = ?1", nativeQuery = true)
 	List<EntidadReserva> obtenerReservasByConsumidor(long id);
 
-	@Query(value="SELECT * FROM Reserva WHERE productor_id = ?1", nativeQuery = true)
+	@Query(value = "SELECT * FROM Reserva WHERE productor_id = ?1", nativeQuery = true)
 	List<EntidadReserva> obtenerReservasByProductor(long id);
 
+	@Query(value = "SELECT ER.nombre, count(r.id) as contador FROM Reserva R JOIN Estado_Reserva ER "
+			+ "		ON R.estado_id = ER.Id "
+			+ "WHERE R.Fecha BETWEEN DATE_ADD(CURDATE(), INTERVAL -90 DAY) AND CURDATE() AND R.productor_id =?1 "
+			+ "GROUP BY ER.nombre", nativeQuery = true)
+	List<Object[]> obtenerMetricasReservasPorEstado(long usuario_id);
 }
