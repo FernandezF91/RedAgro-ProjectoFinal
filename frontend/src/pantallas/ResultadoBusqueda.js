29697de20151ef1cf932609ef96cfdd789107e25
Vanesa Molilna <v.e.molina@hotmail.com>
2019-09-23 16:56:33 -0300

    Update Resultado Búsqueda
    Agrego la paginación

--- frontend/src/pantallas/ResultadoBusqueda.js
+++ frontend/src/pantallas/ResultadoBusqueda.js
@@ -1,93 +1,161 @@
 import React, { Component } from 'react';
 import '../diseños/estilosGlobales.css';
 import '../diseños/ResultadoBusqueda.css';
+import Select from 'react-select';
 import Busqueda from './Busqueda';
+import Paginacion from './Paginacion';
+
+const tamañosListado = [
+	{
+		label: "12",
+		value: "12"
+	},
+	{
+		label: "20",
+		value: "20"
+	},
+	{
+		label: "40",
+		value: "40"
+	},
+	{
+		label: "Todo",
+		value: "Todo"
+	},
+];
+var defaultListado = [
+	{
+		label: "12",
+		value: "12"
+	},
+];
 
 class ResultadoBusqueda extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            busqueda: [],
-            productosSeleccionados: this.props.productosSeleccionados,
-        }
-        this.mostrarPantallaPrincipal = this.mostrarPantallaPrincipal.bind(this);
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			resultadoBusqueda: [],
+			productosSeleccionados: this.props.productosSeleccionados,
+			tamañoListado: 12, //Valor predeterminado
+			paginaActual: 1,
+		}
+		this.mostrarPantallaPrincipal = this.mostrarPantallaPrincipal.bind(this);
+	}
 
-    mostrarPantallaPrincipal() {
-        this.props.history.push({
-            pathname: '/principalConsumidores',
-            state: { id: this.state.id }
-        })
-    }
+	mostrarPantallaPrincipal() {
+		this.props.history.push({
+			pathname: '/principalConsumidores',
+			state: { id: this.state.id }
+		})
+	}
 
-    componentDidMount() {
-        var path = "http://localhost:3000/redAgro/obtenerProductosProductor?id=1";
-        fetch(path)
-            .catch(err => console.error(err))
-            .then(response => { return response.json(); })
-            .then(data => {
-                this.setState({
-                    busqueda: data.map((item) => {
-                        return {
-                            id: item.id,
-                            categoria: item.producto.categoria,
-                            tipo: item.producto.tipo,
-                            titulo: item.titulo,
-                            descripcion: item.descripcion,
-                            stock: item.stock,
-                            tipoDeUnidad: item.tipo_unidad,
-                            tipoDeProduccion: item.tipo_produccion,
-                            precio: item.precio,
-                            techaDeVencimiento: item.fecha_vencimiento,
-                            tiempoDePreparacion: item.tiempo_preparacion,
-                            cantidad: 0,
-                        }
-                    })
-                })
-            })
-    }
+	nextPage = (pageNumber) => {
+		this.setState({ paginaActual: pageNumber });
+	}
 
-    restarProducto = (position) => {
-        //Falta la validación y actualización por stock
-        let { busqueda } = this.state;
-        var productoSeleccionado = busqueda[position];
-        if ((parseInt(productoSeleccionado.cantidad) - 1) >= 0) {
-            let productoActualizado = [
-                ...productoSeleccionado.cantidad = (parseInt(productoSeleccionado.cantidad) - 1).toString(),
-            ]
-            this.setState({ productoSeleccionado: productoActualizado });
-        }
-    }
+	componentDidMount() {
+		var path = "http://localhost:3000/redAgro/obtenerProductosProductor?id=1";
+		fetch(path)
+			.catch(err => console.error(err))
+			.then(response => { return response.json(); })
+			.then(data => {
+				this.setState({
+					resultadoBusqueda: data.map((item) => {
+						return {
+							id: item.id,
+							categoria: item.producto.categoria,
+							tipo: item.producto.tipo,
+							titulo: item.titulo,
+							descripcion: item.descripcion,
+							stock: item.stock,
+							tipoDeUnidad: item.tipo_unidad,
+							tipoDeProduccion: item.tipo_produccion,
+							precio: item.precio,
+							techaDeVencimiento: item.fecha_vencimiento,
+							tiempoDePreparacion: item.tiempo_preparacion,
+							cantidad: 0,
+						}
+					})
+				})
+			})
+	}
 
-    sumarProducto = (position) => {
-        //Falta la validación y actualización por stock
-        let { busqueda } = this.state;
-        var productoSeleccionado = busqueda[position];
-        let productoActualizado = [
-            ...productoSeleccionado.cantidad = (parseInt(productoSeleccionado.cantidad) + 1).toString(),
-        ]
-        this.setState({ productoSeleccionado: productoActualizado });
-    }
+	restarProducto = (position) => {
+		//Falta la validación y actualización por stock
+		let { resultadoBusqueda } = this.state;
+		var productoSeleccionado = resultadoBusqueda[position];
+		if ((parseInt(productoSeleccionado.cantidad) - 1) >= 0) {
+			let productoActualizado = [
+				...productoSeleccionado.cantidad = (parseInt(productoSeleccionado.cantidad) - 1).toString(),
+			]
+			this.setState({ productoSeleccionado: productoActualizado });
+		}
+	}
 
-    agregarAlCarrito = (position) =>{
-        let { busqueda } = this.state;
-        let { productosSeleccionados } = this.state;
-        var producto = busqueda[position];
-        if( parseInt(producto.cantidad) > 0 ) {
-            this.setState({ productosSeleccionados: productosSeleccionados.push(producto) });
-        }    
-    }
+	sumarProducto = (position) => {
+		//Falta la validación y actualización por stock
+		let { resultadoBusqueda } = this.state;
+		var productoSeleccionado = resultadoBusqueda[position];
+		let productoActualizado = [
+			...productoSeleccionado.cantidad = (parseInt(productoSeleccionado.cantidad) + 1).toString(),
+		]
+		this.setState({ productoSeleccionado: productoActualizado });
+	}
 
-    render() {
-        return (
-            <div>
-                <div className="titulosPrincipales">Resultado Búsqueda</div>
-                <Busqueda listaDeProductos={this.state.busqueda}
-                    sumarProducto={this.sumarProducto}
-                    restarProducto={this.restarProducto} 
-                    agregarAlCarrito={this.agregarAlCarrito}/>
-            </div>
-        )
-    }
+	agregarAlCarrito = (position) => {
+		let { resultadoBusqueda } = this.state;
+		let { productosSeleccionados } = this.state;
+		var producto = resultadoBusqueda[position];
+		if (parseInt(producto.cantidad) > 0) {
+			this.setState({ productosSeleccionados: productosSeleccionados.push(producto) });
+		}
+	}
+
+	actualizarTamañoListado = (tamaño) => {
+		let actualizarListado = [];
+		if (tamaño.value === "Todo") {
+			this.setState({ tamañoListado: this.state.resultadoBusqueda.length })
+		} else {
+			this.setState({ tamañoListado: tamaño.value })
+		}
+		actualizarListado.push(tamaño);
+		defaultListado = actualizarListado;
+	}
+
+	render() {
+		const { resultadoBusqueda, paginaActual, tamañoListado } = this.state;
+		const numberOfPages = Math.ceil(resultadoBusqueda.length / tamañoListado);
+		const indexOfLastReserva = paginaActual * tamañoListado;
+		const indexOfFirstReserva = indexOfLastReserva - tamañoListado;
+		const listadoBusqueda = resultadoBusqueda.slice(indexOfFirstReserva, indexOfLastReserva);
+
+		return (
+			<div>
+				<div className="titulosPrincipales">Resultado Búsqueda</div>
+				{
+					resultadoBusqueda.length > 0 ?
+						<div className="opcionesCantidad">
+							<Select className="cantidadProductos"
+								value={defaultListado}
+								options={tamañosListado}
+								onChange={nuevoTamaño => this.actualizarTamañoListado(nuevoTamaño)} />
+						</div>
+						: ''
+				}
+				<Busqueda listaDeProductos={listadoBusqueda}
+					sumarProducto={this.sumarProducto}
+					restarProducto={this.restarProducto}
+					agregarAlCarrito={this.agregarAlCarrito} />
+				{
+					resultadoBusqueda.length > tamañoListado ?
+						<Paginacion
+							pages={numberOfPages}
+							nextPage={this.nextPage}
+							currentPage={this.state.paginaActual} />
+						: ''
+				}
+			</div>
+		)
+	}
 }
 export default ResultadoBusqueda;
