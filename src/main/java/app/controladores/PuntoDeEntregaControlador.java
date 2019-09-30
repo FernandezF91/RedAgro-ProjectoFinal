accca5d721561e0c6f05364c181c8f5b8c59abb7
Vanesa Molina <vmolina@grupoassa.com>
2019-09-29 21:16:18 -0300

    Update Checkout
    Agrego búsqueda de puntos de entrega

--- src/main/java/app/controladores/PuntoDeEntregaControlador.java
+++ src/main/java/app/controladores/PuntoDeEntregaControlador.java
@@ -7,10 +7,12 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.clases.PuntoEntrega;
 import app.clases.Usuario;
+import app.clases.Productor;
 import app.daos.ProductorDao;
 import app.daos.PuntoEntregaDao;
 import app.mappers.PuntoEntregaMapper;
@@ -39,6 +41,20 @@ public class PuntoDeEntregaControlador {
 		
 		return puntos_entrega;		
 	
+	}
+	
+	@CrossOrigin(origins = "http://localhost:3000")
+	@GetMapping(path = "redAgro/ptos_entrega_productores")
+	public List<PuntoEntrega> obtenerListadoPuntosEntrega(@RequestParam List<Long> productores) {
+
+		PuntoEntregaMapper punto_entrega_mapper = new PuntoEntregaMapper();
+		List<EntidadPuntoEntrega> entidad_puntos = new ArrayList<EntidadPuntoEntrega>();
+		List<PuntoEntrega> puntos_entrega = new ArrayList<PuntoEntrega>();
+		
+		entidad_puntos = puntoEntregaDAO.obtenerPuntosEntregaProductores(productores);
+        puntos_entrega = entidad_puntos.stream().map(entidad -> punto_entrega_mapper.mapFromEntity(entidad)).collect(Collectors.toList());
+		
+		return puntos_entrega;		
 	}
 
 }
--- src/main/java/app/daos/PuntoEntregaDao.java
+++ src/main/java/app/daos/PuntoEntregaDao.java
@@ -5,10 +5,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import app.modelos.EntidadPuntoEntrega;
+import app.modelos.EntidadProductor;
 
 public interface PuntoEntregaDao extends JpaRepository<EntidadPuntoEntrega, Long>{
+	@Query("select p from EntidadPuntoEntrega as p where p.id IN (:productores)")
 	
+	List<EntidadPuntoEntrega> obtenerPuntosEntregaProductores(@Param("productores") List<Long> productores);
 	
 }
