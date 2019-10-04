3400a4689454aa3a63c89c04b6dc68ffe738dffb
Vanesa Molina <vmolina@grupoassa.com>
2019-10-04 00:26:42 -0300

    Update Back ptos de entrega
    Agrego busqueda puntos de entrega activos.

--- src/main/java/app/daos/PuntoEntregaDao.java
+++ src/main/java/app/daos/PuntoEntregaDao.java
@@ -18,8 +18,11 @@ public interface PuntoEntregaDao extends JpaRepository<EntidadPuntoEntrega, Long
 	@Query("select p from EntidadPuntoEntrega as p where p.id IN (:productores)")	
 	List<EntidadPuntoEntrega> obtenerPuntosEntregaProductores(@Param("productores") List<Long> productores);
 	
-	@Query("select pe from EntidadPuntoEntrega pe where pe.productor=?1")
+	@Query("select pe from EntidadPuntoEntrega pe where pe.productor=?1 and pe.activo=1")
 	List<EntidadPuntoEntrega> obtenerPuntosEntregaProductor(EntidadProductor productor);
+	
+	@Query("select pe from EntidadPuntoEntrega pe where pe.productor=?1")
+	List<EntidadPuntoEntrega> obtenerPuntosEntregaActivos(EntidadProductor productor);
 	
 	@Query("select pe from EntidadPuntoEntrega pe where pe.activo=1")
 	List<EntidadPuntoEntrega> obtenerTodosLosPuntos();
