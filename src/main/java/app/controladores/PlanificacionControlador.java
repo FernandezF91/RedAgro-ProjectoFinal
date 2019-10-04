0b7201ae40d3e113a912b6f200496801a3b2567d
ezebosso <eze.bosso@gmail.com>
2019-10-03 23:09:43 -0300

    Back planificacion

---
+++ src/main/java/app/controladores/PlanificacionControlador.java
@@ -0,0 +1,18 @@
+package app.controladores;
+
+import smile.*;
+
+public class PlanificacionControlador {
+			
+}
+//	@Autowired
+//	HistoricoDao historicoDao;
+//	
+//	@CrossOrigin(origins = "http://localhost:3000")
+//	@PostMapping(path = "redAgro/uploadFile")
+//	public void submit(@RequestParam("file") MultipartFile file) throws IOException {
+//	HistoricoMapper mapper = new HistoricoMapper();
+//	ArrayList<EntidadHistorico> historicos = mapper.mapToEntity(file);
+//	historicos.forEach(h -> historicoDao.save(h));
+//	//insertarHistorico(h.getCantidad_vendida(), h.getTipo_certificacion(), h.getTipo_produccion(), h.getProducto().getId(), h.getProductor().getId()));
+//	}
