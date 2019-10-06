0f5acbd64b6d10506e5920ea284dff9a1ffb94a7
barbsabakdanian <50269124+barbsabakdanian@users.noreply.github.com>
2019-10-06 16:10:22 -0300

    Update ProductoProductorDao.java
    Busqueda cuando el stock es mayor a 0 o el vencimiento de hoy en adelante

--- src/main/java/app/daos/ProductoProductorDao.java
+++ src/main/java/app/daos/ProductoProductorDao.java
@@ -14,9 +14,11 @@ public interface ProductoProductorDao extends JpaRepository<EntidadProductoProdu
 	List<EntidadProductoProductor> obtenerProductosByProductor(long id);
 
 	@Query(value = "SELECT distinct p.* FROM Producto_Productor p JOIN Producto po ON p.producto_id = po.id "
-			+ " WHERE (p.titulo LIKE %:titulo% OR po.tipo LIKE %:titulo%) UNION DISTINCT "
-			+ " SELECT distinct p.* FROM Producto_Productor p JOIN Productor pr ON P.Productor_id = pr.usuario_id "
-			+ " WHERE pr.razon_social LIKE %:titulo%", nativeQuery = true)
+			+ " WHERE (p.titulo LIKE %:titulo% OR po.tipo LIKE %:titulo%) "
+			+ " AND p.stock > 0 AND p.fecha_vencimiento >= CURDATE() UNION DISTINCT "
+			+ " SELECT distinct p.* FROM Producto_Productor p "
+			+ " JOIN Productor pr ON P.Productor_id = pr.usuario_id "
+			+ " WHERE pr.razon_social LIKE %:titulo%  AND p.stock > 0 AND p.fecha_vencimiento >= CURDATE() ", nativeQuery = true)
 	List<EntidadProductoProductor> obtenerProductos(@Param("titulo") String titulo);
 
 	@Query(value = "SELECT p.stock FROM Producto_Productor p where p.id = ?1", nativeQuery = true)
