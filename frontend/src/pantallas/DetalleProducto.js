79b3a71657f75b61a1485fd7cc69537d34d8f7c6
Vanesa Molina <vmolina@grupoassa.com>
2019-10-14 18:13:17 -0300

    Update Detalle Producto
    Fix botones

--- frontend/src/pantallas/DetalleProducto.js
+++ frontend/src/pantallas/DetalleProducto.js
@@ -14,8 +14,6 @@ class DetalleBusqueda extends Component {
             showModal: false,
         }
 
-        this.sumarProducto = this.sumarProducto.bind(this);
-
     }
 
     restarProducto = () => {
@@ -39,17 +37,17 @@ class DetalleBusqueda extends Component {
             }
         });
     }
-    
+
     render() {
-        const sumarProducto = this.sumarProducto;
-        const restarProducto = this.restarProducto;
+        const sumarProducto =  () => this.sumarProducto();
+        const restarProducto = () => this.restarProducto();
         return (
             <Container className="containerPrincipal">
                 <Row>
                     <Col className="imagenProducto" sm={8}>
                         <Carousel >
                             {
-                                this.props.producto.imagenes.map((imagen, index) => (
+                                this.state.producto.imagenes.map((imagen, index) => (
                                     <Carousel.Item key={index}>
                                         <img
                                             className="imagenesBusqueda"
@@ -65,39 +63,39 @@ class DetalleBusqueda extends Component {
                     </Col>
                     <Col className="detalleDelProducto" sm={4}>
                         <br />
-                        <h6 className="grey-text">{this.props.producto.categoria}</h6>
-                        <h3 className="tituloProducto">{this.props.producto.titulo}</h3>
+                        <h6 className="grey-text">{this.state.producto.categoria}</h6>
+                        <h3 className="tituloProducto">{this.state.producto.titulo}</h3>
                         <h3 className="precioProducto">
-                            <NumberFormat value={this.props.producto.precio} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} /></h3>
-                        x {this.props.producto.tipoDeUnidad}
+                            <NumberFormat value={this.state.producto.precio} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} /></h3>
+                        x {this.state.producto.tipoDeUnidad}
 
                         <div className="descripcionProducto">
                             Producido por:
-                        <h6>{this.props.producto.productor.razon_social}</h6>
+                        <h6>{this.state.producto.productor.razon_social}</h6>
                         </div>
 
                         <div className="descripcionProducto">
                             Descripción del producto:
-                        <h6>{this.props.producto.descripcion}</h6>
+                        <h6>{this.state.producto.descripcion}</h6>
                         </div>
                         <div className="descripcionProducto">
                             Tipo de producción:
-                        <h6>{this.props.producto.tipoDeProduccion}</h6>
+                        <h6>{this.state.producto.tipoDeProduccion}</h6>
                         </div>
                         {
-                            this.props.producto.fechaDeVencimiento !== undefined ?
+                            this.state.producto.fechaDeVencimiento !== undefined ?
                                 <div className="descripcionProducto">
-                                    Fecha de vencimiento: <h6>{moment(this.props.producto.fechaDeVencimiento, 'YYYY-MM-DD').format("DD/MM/YYYY")}</h6>
+                                    Fecha de vencimiento: <h6>{moment(this.state.producto.fechaDeVencimiento, 'YYYY-MM-DD').format("DD/MM/YYYY")}</h6>
                                 </div>
                                 : ''
                         }
                         <div className="botonesDetalle">
-                            <button className="iconosListado" onClick={() => restarProducto()}
+                            <button className="iconosListado" onClick={restarProducto}
                             >
                                 <i className="fas fa-minus" />
                             </button>
-                            <span className="float-center">{this.props.producto.cantidad}</span>
-                            <button className="iconosListado" onClick={() => sumarProducto}
+                            <span className="float-center">{this.state.producto.cantidad}</span>
+                            <button className="iconosListado" onClick={sumarProducto}
                             >
                                 <i className="fas fa-plus" />
                             </button>
