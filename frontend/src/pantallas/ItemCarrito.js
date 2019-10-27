33ab3022db822fbb184df38bb33cbd1856cac998
barbsabakdanian <50269124+barbsabakdanian@users.noreply.github.com>
2019-10-26 23:24:18 -0300

    Checkout

--- frontend/src/pantallas/ItemCarrito.js
+++ frontend/src/pantallas/ItemCarrito.js
@@ -5,6 +5,7 @@ import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Image from 'react-bootstrap/Image';
 import '../diseños/Carrito.css';
+import '../diseños/estilosGlobales.css';
 
 const ItemCarrito = ({ listaDeProductos, sumarProducto, restarProducto, quitarProducto, getTotalCarrito, validarItemsCarrito }) => {
 
@@ -36,107 +37,109 @@ const ItemCarrito = ({ listaDeProductos, sumarProducto, restarProducto, quitarPr
     ]
 
     return (
-        <MDBTable responsive hover>
-            <MDBTableHead columns={columns} />
-            <MDBTableBody>
-                {
-                    listaDeProductos.map((producto, index) =>
-                        <tr key={producto.id}>
-                            <td>
-                                <Image
-                                    roundedCircle
-                                    alt="ImagenCarrito"
-                                    src={"data:" + producto.imagenes[0].tipo_contenido + ";base64," + producto.imagenes[0].image}
-                                    mode='fit'
-                                    height="80px" width="auto" />
-                            </td>
-                            <td>
-                                <h5>{producto.titulo}</h5>
-                                {producto.descripcion}
-                                <br />
-                                Producido por {producto.productor.razon_social}
-                            </td>
-                            {
-                                (producto.cantidad > producto.stock) ?
-                                    <td>
-                                        <button className="iconosListado" onClick={() => restarProducto(index)}>
-                                            <i className="fas fa-minus" />
-                                        </button>
-                                        <span className="item-quantity-no-stock" >{producto.cantidad}</span>
-                                        <button className="iconosListado" onClick={() => sumarProducto(index)} disabled>
-                                            <i className="fas fa-plus" />
-                                        </button>
-                                    </td>
-                                    :
-                                    <td>
-                                        <button className="iconosListado" onClick={() => restarProducto(index)}>
-                                            <i className="fas fa-minus" />
-                                        </button>
-                                        <span className="item-quantity">{producto.cantidad}</span>
-                                        {
-                                            (producto.cantidad === producto.stock) ?
-                                                <button className="iconosListado" onClick={() => sumarProducto(index)} disabled>
-                                                    <i className="fas fa-plus" />
-                                                </button>
+        <div>
+            <MDBTable responsive hover>
+                <MDBTableHead columns={columns} />
+                <MDBTableBody>
+                    {
+                        listaDeProductos.map((producto, index) =>
+                            <tr key={producto.id}>
+                                <td>
+                                    <Image
+                                        roundedCircle
+                                        alt="ImagenCarrito"
+                                        src={"data:" + producto.imagenes[0].tipo_contenido + ";base64," + producto.imagenes[0].image}
+                                        mode='fit'
+                                        height="80px" width="auto" />
+                                </td>
+                                <td>
+                                    <h5>{producto.titulo}</h5>
+                                    {producto.descripcion}
+                                    <br />
+                                    Producido por {producto.productor.razon_social}
+                                </td>
+                                {
+                                    (producto.cantidad > producto.stock) ?
+                                        <td>
+                                            <button className="iconosListado" onClick={() => restarProducto(index)}>
+                                                <i className="fas fa-minus" />
+                                            </button>
+                                            <span className="item-quantity-no-stock" >{producto.cantidad}</span>
+                                            <button className="iconosListado" onClick={() => sumarProducto(index)} disabled>
+                                                <i className="fas fa-plus" />
+                                            </button>
+                                        </td>
+                                        :
+                                        <td>
+                                            <button className="iconosListado" onClick={() => restarProducto(index)}>
+                                                <i className="fas fa-minus" />
+                                            </button>
+                                            <span className="item-quantity">{producto.cantidad}</span>
+                                            {
+                                                (producto.cantidad === producto.stock) ?
+                                                    <button className="iconosListado" onClick={() => sumarProducto(index)} disabled>
+                                                        <i className="fas fa-plus" />
+                                                    </button>
+                                                    :
+                                                    <button className="iconosListado" onClick={() => sumarProducto(index)}>
+                                                        <i className="fas fa-plus" />
+                                                    </button>
+                                            }
+                                        </td>
+                                }
+                                <td>
+                                    {
+                                        (producto.oferta === null || producto.oferta === undefined) ?
+                                            <NumberFormat value={producto.precio} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} />
+                                            :
+                                            (producto.oferta.activo) ?
+                                                <div title="Producto en oferta!">
+                                                    <strike>
+                                                        <NumberFormat value={producto.precio} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} />
+                                                    </strike>
+                                                    <br />
+                                                    <NumberFormat value={producto.precio - producto.precio * producto.oferta.porcentaje / 100} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} />
+                                                </div>
                                                 :
-                                                <button className="iconosListado" onClick={() => sumarProducto(index)}>
-                                                    <i className="fas fa-plus" />
-                                                </button>
-                                        }
-                                    </td>
-                            }
-                            <td>
-                                {
-                                    (producto.oferta === null || producto.oferta === undefined) ?
-                                        <NumberFormat value={producto.precio} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} />
-                                        :
-                                        (producto.oferta.activo) ?
-                                            <div title="Producto en oferta!">
-                                                <strike>
+                                                <div>
                                                     <NumberFormat value={producto.precio} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} />
-                                                </strike>
-                                                <br />
-                                                <NumberFormat value={producto.precio - producto.precio * producto.oferta.porcentaje / 100} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} />
-                                            </div>
+                                                </div>
+                                    }
+                                </td>
+                                <td>
+                                    {
+                                        (producto.oferta === null || producto.oferta === undefined) ?
+                                            <NumberFormat value={producto.precio * producto.cantidad} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} />
                                             :
-                                            <div>
-                                                <NumberFormat value={producto.precio} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} />
-                                            </div>
-                                }
-                            </td>
-                            <td>
-                                {
-                                    (producto.oferta === null || producto.oferta === undefined) ?
-                                        <NumberFormat value={producto.precio * producto.cantidad} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} />
-                                        :
-                                        (producto.oferta.activo) ?
-                                            <NumberFormat value={(producto.precio - producto.precio * producto.oferta.porcentaje / 100) * producto.cantidad} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} />
-                                            :
-                                            <div>
-                                                <NumberFormat value={producto.precio * producto.cantidad} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} />
-                                            </div>
-                                }
-                            </td>
-                            <td>
-                                <button className="remove" onClick={() => quitarProducto(index)}>
-                                    <i className="fas fa-times" />
-                                </button>
-                            </td>
-                        </tr>
-                    )
-                }
-                <tr>
-                    <td colSpan="6" align="right">
-                        <h5>Total
-                           <NumberFormat value={getTotalCarrito(listaDeProductos)} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix=" $ " decimalScale={2} fixedDecimalScale={true} />
-                        </h5>
-                        <Link to={'/principalConsumidores/Checkout'}>
-                            <Button variant="success" type="submit" onClick={() => validarItemsCarrito()}>Finalizar Reserva</Button>
-                        </Link>
-                    </td>
-                </tr>
-            </MDBTableBody>
-        </MDBTable>
+                                            (producto.oferta.activo) ?
+                                                <NumberFormat value={(producto.precio - producto.precio * producto.oferta.porcentaje / 100) * producto.cantidad} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} />
+                                                :
+                                                <div>
+                                                    <NumberFormat value={producto.precio * producto.cantidad} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} />
+                                                </div>
+                                    }
+                                </td>
+                                <td>
+                                    <button className="remove" onClick={() => quitarProducto(index)}>
+                                        <i className="fas fa-times" />
+                                    </button>
+                                </td>
+                            </tr>
+                        )
+                    }
+                </MDBTableBody>
+            </MDBTable>
+            <h5 className="precioCheckout">
+                Total <NumberFormat value={getTotalCarrito(listaDeProductos)} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix=" $ " decimalScale={2} fixedDecimalScale={true} />
+            </h5>
+            <br />
+            <div>
+                <Button variant="light" type="submit" href="javascript:history.back()" className="botonIzquierda">Atras</Button>
+                <Link to={'/principalConsumidores/Checkout'}>
+                    <Button variant="success" type="submit" onClick={() => validarItemsCarrito()} className="botonDerecha">Finalizar Reserva</Button>
+                </Link>
+            </div>
+        </div>
     )
 }
 export default ItemCarrito;
