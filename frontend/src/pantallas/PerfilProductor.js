79e81b993fe620ebe27ae55c76479b73c31cd9b2
Vanesa Molina <vmolina@grupoassa.com>
2019-10-29 09:10:16 -0300

    Update Perfil Productor

--- frontend/src/pantallas/PerfilProductor.js
+++ frontend/src/pantallas/PerfilProductor.js
@@ -1,8 +1,16 @@
+import '../diseños/PerfilProductor.css'
+import '../diseños/estilosGlobales.css';
 import React, { Component } from 'react';
 import Loader from 'react-loader-spinner';
-import '../diseños/PerfilProductor.css'
 import Avatar from '@material-ui/core/Avatar';
-import { Navbar, Nav, Row, InputGroup, FormControl, Container } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
+import { MDBCard, MDBCardTitle, MDBCardBody, MDBCardImage, MDBCardText, MDBCol, MDBRow, MDBCarousel, MDBCarouselItem, MDBCarouselInner, MDBContainer } from "mdbreact";
+import Carousel from 'react-bootstrap/Carousel';
+import Paginacion from './Paginacion';
+import NumberFormat from 'react-number-format';
+import moment from 'moment';
+
+moment.locale('es');
 
 class PerfilProductor extends Component {
     constructor(props) {
@@ -19,6 +27,9 @@ class PerfilProductor extends Component {
                 fechasEntrega: {},
             },
             loading: true,
+            paginaActual: 1,
+            productosPerPage: 3,
+            tamañoListado: 3,
         }
     }
 
@@ -57,6 +68,7 @@ class PerfilProductor extends Component {
                             razon_social: data.razon_social,
                             nombre: data.usuario.nombre + " " + data.usuario.apellido,
                             telefono: data.telefono,
+                            fecha_creacion: data.usuario.fecha_creacion,
                             puntosEntrega: data.puntosEntrega.map(item => {
                                 return {
                                     pais: item.pais,
@@ -133,6 +145,67 @@ class PerfilProductor extends Component {
             })
     }
 
+    nextPage = (pageNumber) => {
+        this.setState({ paginaActual: pageNumber });
+    }
+
+    generarProductosAMostrar = (item) => {
+        return (
+            <MDBCol md="4" key={item.id}>
+                <MDBCard className="mb-2">
+                    <MDBCardImage
+                        className="imagenesBusqueda"
+                        src={"data:" + item.imagenes[0].tipo_contenido + ";base64," + item.imagenes[0].image}
+                        alt="ImagenBusqueda"
+                        overlay="white-slight"
+                        height="150x" width="auto" />
+
+                    <MDBCardBody className="text-center">
+                        <h6 className="grey-text">{item.tipo}</h6>
+                        <MDBCardTitle>
+                            <strong className="dark-grey-text">{item.titulo}</strong>
+                        </MDBCardTitle>
+                        <MDBCardText>
+                            <strong className="float-center">
+                                {
+                                    (item.oferta === null || item.oferta === undefined) ?
+                                        <div>
+                                            <NumberFormat value={item.precio} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} /> x {item.tipoDeUnidad}
+                                        </div>
+                                        :
+                                        (item.oferta.activo) ?
+                                            <div title="Producto en oferta!">
+                                                <strike>
+                                                    <NumberFormat value={item.precio} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} /> x {item.tipoDeUnidad}
+                                                </strike>
+                                                <br />
+                                                <NumberFormat value={item.precio - item.precio * item.oferta.porcentaje / 100} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} /> x {item.tipoDeUnidad}
+                                            </div>
+                                            :
+                                            <div>
+                                                <NumberFormat value={item.precio} displayType={'text'} thousandSeparator={"."} decimalSeparator={","} prefix="$ " decimalScale={2} fixedDecimalScale={true} /> x {item.tipoDeUnidad}
+                                            </div>
+                                }
+                            </strong>
+                        </MDBCardText>
+                    </MDBCardBody>
+                </MDBCard>
+            </MDBCol>
+        )
+    }
+
+    crearListaDeProductos(numberOfPages, productosPerPage, productos) {
+        let nuevaLista = [];
+        var indexOfLastProducto, indexOfFirstProducto;
+        for (let i = 1; i <= numberOfPages; i++) {
+            indexOfLastProducto = i * productosPerPage;
+            indexOfFirstProducto = indexOfLastProducto - productosPerPage;
+            let lista = productos.slice(indexOfFirstProducto, indexOfLastProducto)
+            nuevaLista.push(lista);
+        }
+        return nuevaLista;
+    }
+
     render() {
 
         if (this.state.loading)
@@ -143,10 +216,45 @@ class PerfilProductor extends Component {
                 width={150}
                 className="loader" />;
 
+        var fecha = moment(this.state.datosProductor.fecha_creacion, "YYYY-MM-DD").fromNow().toString();
+        fecha = fecha.charAt(0).toUpperCase() + fecha.slice(1);
+        const productos = this.state.datosProductor.productos;
+        const { productosPerPage } = this.state;
+        const numberOfPages = Math.ceil(productos.length / productosPerPage);
+        let lista = this.crearListaDeProductos(numberOfPages, productosPerPage, productos);
+
         return (
-            <div>
-                <div className="nombreDelPerfil">{this.state.datosProductor.nombre} </div>
+            <div className="perfilProductor">
+                <Row>
+                    <div className="nombreDelPerfil">
+                        {this.state.datosProductor.nombre}
+                        <h6 className="grey-text">{fecha} que forma parte de Cultura Verde</h6>
+                    </div>
+                </Row>
+
+                <MDBContainer>
+                <div className="listadoDeProductos">Mis productos</div>
+                    <MDBCarousel activeItem={1} length={lista.length} slide={true} showControls={true} showIndicators={true} multiItem>
+                        <MDBCarouselInner>
+                            {
+                                lista.map((itemLista, index) => (
+                                    <MDBCarouselItem itemId={index + 1}>
+                                        <MDBRow>
+                                            {
+                                                itemLista.map((item) => (
+                                                    this.generarProductosAMostrar(item)
+                                                ))
+                                            }
+                                        </MDBRow>
+                                    </MDBCarouselItem>
+                                ))
+                            }
+                        </MDBCarouselInner>
+                    </MDBCarousel>
+                </MDBContainer>
+
             </div>
+
         );
     }
 }
