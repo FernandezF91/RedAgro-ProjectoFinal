7bf445ac7e72577bd0b34fee9dff25e777712d3a
Vanesa Molina <vmolina@grupoassa.com>
2019-10-31 00:22:49 -0300

    Update MailConfirmacion.java
    Fix mayusculas

--- src/main/java/app/clases/MailConfirmacion.java
+++ src/main/java/app/clases/MailConfirmacion.java
@@ -8,13 +8,13 @@ public class MailConfirmacion extends MailSender {
 	public MailConfirmacion(String to, Long id) {
 
 		this.setTo(to);
-		this.setContenido_email("<div> <h1>Bienvenido a culturaVerde!</h1>"
+		this.setContenido_email("<div> <h1>Bienvenido a Cultura Verde!</h1>"
 				+ "Gracias por registrarte en nuestra sistema." + "<br>" + "<br>"
 				+ "Para poder utilizar nuestra página, confirmá tu cuenta por favor." + "<br>" + "<br>" + "<a href="
 				+ "http://localhost:3000/confirmar_cuenta/" + id + ">" + "<button " + "class=" + "button " + "style="
 				+ "background-color:#4CAF50; border:none; color:white; padding: 15px 32px; text-align: center; text-decoration: none>"
 				+ "Confirmar" + "</button>" + "</a><div>" + "<br>" + "<br>" + "Saludos," + "<br>"
-				+ "Equipo de CulturaVerde"
+				+ "Equipo de Cultura Verde"
 
 		);
 		this.setAsunto("Confirmación de cuenta");
