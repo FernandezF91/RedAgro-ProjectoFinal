93444424c0bf81d5ee476e79dbb74a990e764e24
Vanesa Molina <vmolina@grupoassa.com>
2019-11-03 00:36:56 -0300

    Actualización razón social

--- src/main/java/app/daos/ProductorDao.java
+++ src/main/java/app/daos/ProductorDao.java
@@ -1,7 +1,9 @@
 package app.daos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import app.modelos.EntidadProductor;
 
@@ -9,5 +11,10 @@ public interface ProductorDao extends JpaRepository<EntidadProductor, Long> {
 
 	@Query("select p from EntidadProductor p where p.id = ?1")
 	EntidadProductor obtenerProductor(long id);
+	
+	@Transactional
+	@Modifying
+	@Query("UPDATE EntidadProductor u SET u.razon_social =?1 WHERE u.id =?2")
+	void actualizarRazonSocial(String razon_social, Long id);
 	
 }
