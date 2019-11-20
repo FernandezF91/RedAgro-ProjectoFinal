448e78367dc94e4ddf0071f314c25630aa9bac49
barbsabakdanian <50269124+barbsabakdanian@users.noreply.github.com>
2019-11-20 02:18:46 -0300

    Cargar historico front / validaciones

--- frontend/src/pantallas/CargarHistorico.js
+++ frontend/src/pantallas/CargarHistorico.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { FilePond } from 'react-filepond';
 // Import FilePond styles
 import 'filepond/dist/filepond.min.css';
-import { MDBModal } from 'mdbreact';
+import { MDBModal, MDBCard, MDBRow, MDBCol } from 'mdbreact';
 import Loader from 'react-loader-spinner';
 
 import { Button } from 'react-bootstrap';
@@ -14,10 +14,11 @@ import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 class CargarHistorico extends Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
 			files: "",
 			mensaje: "",
-			formOK: "",
+			validaciones: [],
 			loading: false,
 			resultadoRequest: 0,
 			showModal: false,
@@ -26,8 +27,8 @@ class CargarHistorico extends Component {
 		this.subirArchivos = this.subirArchivos.bind(this);
 		this.validarCampos = this.validarCampos.bind(this);
 		this.cerrarModal = this.cerrarModal.bind(this);
+		this.cerrarModalError = this.cerrarModalError.bind(this);
 		this.mostrarPantallaPrincipal = this.mostrarPantallaPrincipal.bind(this);
-
 	}
 
 	mostrarPantallaPrincipal() {
@@ -40,35 +41,42 @@ class CargarHistorico extends Component {
 	}
 
 	validarCampos() {
+		var showModal = false;
+		this.setState({
+			validaciones: []
+		});
+		let validaciones = [];
 
 		if (this.state.files.length === 0) {
-
-			this.setState({
-				showModal: true,
-				mensaje: "Tenés que cargar un archivo",
-				formOK: false,
-			});
-
-			return false
+			validaciones["archivo"] = "Campo requerido";
+			showModal = true;
 		}
 
-		return true
+		if (showModal) {
+			this.setState({
+				validaciones: validaciones,
+				showModal: showModal,
+				mensaje: "Ups! No seleccionaste ningún archivo",
+				loading: false,
+				resultadoRequest: 0
+			});
+			return false;
+		} else {
+			return true;
+		}
 	}
 
 	subirArchivos() {
-		
 		var _this = this
 
 		this.setState({
-            loading: true
+			loading: true
 		});
-		
+
 		var file = _this.state.files[0]
-		const path = "http://"+window.$ip+":3000/redAgro/uploadFile";
+		const path = "http://" + window.$ip + ":3000/redAgro/uploadFile";
 
 		if (_this.validarCampos()) {
-
-
 			var formato = file.name.split(".")
 
 			if (formato[1] === "csv") {
@@ -86,59 +94,56 @@ class CargarHistorico extends Component {
 
 						_this.setState({
 							showModal: true,
-							mensaje: "Ocurrió algún error inesperado. Intenta nuevamente",
-							formOK: false,
-							resultadoRequest: false,
-							loading:false
+							mensaje: "Ocurrió un error al procesar el archivo. Por favor, reintentá en unos minutos",
+							resultadoRequest: 0,
+							loading: false
 						});
 						return;
 					}
 					response.text().then(
 						function (response) {
-
 							_this.setState({
 								showModal: true,
-								mensaje: "El archivo se guardo correctamente",
-								formOK: true,
-								resultadoRequest: true,
-								loading:false
+								mensaje: "Archivo importado correctamente!",
+								resultadoRequest: 200,
+								loading: false
 							});
-
 						});
-
 				});
 
 			} else {
+				this.setState({
+					validaciones: []
+				});
+				let validaciones = [];
+
+				validaciones["archivo"] = "Formato inválido";
 
 				_this.setState({
-					showModal: true,
-					mensaje: "El archivo debe ser .csv",
-					formOK: false,
-					loading:false
+					validaciones: validaciones,
+					mensaje: "Ups! El formato del archivo no es el correcto",
+					loading: false,
+					resultadoRequest: 0,
+					showModal: true
 				});
 			}
-
 		}
-
 	}
 
-
 	cerrarModal() {
-
-		if (this.state.formOK === true) {
-
-			this.mostrarPantallaPrincipal()
-
-		}
 		this.setState({
 			showModal: false,
-			loading:false
+		})
+		this.mostrarPantallaPrincipal();
+	}
+
+	cerrarModalError() {
+		this.setState({
+			showModal: false
 		})
 	}
 
-
 	render() {
-
 		if (this.state.loading) return (
 			<Loader
 				type="Grid"
@@ -150,27 +155,35 @@ class CargarHistorico extends Component {
 		)
 
 		return (
-			<div>
-				
+			<div className="container">
 				<div className="titulosPrincipales">Cargar archivos históricos</div>
-
-				<span md="3">En esta sección se podrán cargar archivos del tipo "valores separados por coma(ejemplo.csv)". </span>
-				<br></br>
-				<span md="3">Un ejemplo de cómo debe ser dicho archivo es el siguiente.</span><br></br>
-				<span md="3">frutas,Banana,100,1,CABA,2018</span><br></br>
-				<span md="3">frutas,Manzana,500,2,CABA,2018</span><br></br>
-				<span md="3">Dónde los valores corresponden respectivamente a:</span><br></br>
-				<span md="3">categoría, producto, cantidad vendida, mes, zona de venta, año</span><br></br>
-				<br></br>
-				<br></br>
-				<div><h5>Seleccione los archivos históricos a cargar</h5></div>
-
+				<MDBCard className="boxSinPreferencias mb-4 py-3">
+					<i className="fas fa-info-circle iconoBoxGris" />
+					<ul className="listaInstruccionesHistorico">
+						<li>Desde esta sección, se podrán importar archivos con el formato CSV (Valores separados por coma). Por ejemplo, "MiArchivo.csv".</li>
+						<li>Los datos que se deben incluir en el mismo son los siguientes: Categoría, Producto, Cantidad vendida, Mes, Zona de venta y año.</li>
+						<li>El siguiente es un ejemplo de como se debería ver el archivo:
+						<ul>
+								<li>Frutas,Banana,100,1,CABA,2018</li>
+								<li>Frutas,Banana,100,1,CABA,2018</li>
+							</ul>
+						</li>
+					</ul>
+				</MDBCard>
+				<MDBRow className="justifyContentCenter">
+					<span md="3">Archivo a importar</span>
+					{
+						(this.state.validaciones["archivo"]) &&
+						<i className="fa fa-exclamation-circle mensajeErrorForm" />
+					}
+				</MDBRow>
+				<br />
 				<FilePond
 					className="cursorManito cajaImagenesWidth"
 					ref="filep"
 					allowMultiple={false}
 					maxFiles={1}
-					labelIdle={"Arrastre o suba sus archivos aquí"}
+					labelIdle={"Arrastre o suba su archivo aquí"}
 					onupdatefiles={(fileItems) => {
 						// Set current file objects to this.state
 						this.setState({
@@ -178,19 +191,33 @@ class CargarHistorico extends Component {
 						});
 					}}
 				/>
-				<div>
-					<br></br>
+				{
+					(this.state.validaciones["archivo"]) ?
+						<MDBRow center className="cajaImagenesWidth">
+							<MDBCol md="6">
+								<div className="mensajeErrorCampos">{this.state.validaciones["archivo"]}</div>
+							</MDBCol>
+							<MDBCol md="6">
+								<div className="condicionesInputs">(*) 1 archivo como máximo</div>
+							</MDBCol>
+						</MDBRow>
+						:
+						<MDBRow>
+							<div className="condicionesInputs col-md-8 cajaImagenesWidth">(*) 1 archivo como máximo</div>
+						</MDBRow>
+				}
+				<div className="botones">
 					<Button variant="light" onClick={this.mostrarPantallaPrincipal}>Cancelar</Button>
 					<Button variant="success" onClick={() => this.subirArchivos()}>Guardar</Button>
 				</div>
 				{
 					<MDBModal isOpen={this.state.showModal} centered size="sm">
 						<div className="modalMargenes" tabIndex="0">
-							<i className="fas fa-times botonCerrarModal cursorManito" onClick={this.cerrarModal} />
-							<br />
-							{(this.state.resultadoRequest === true) ?
+							{(this.state.resultadoRequest === 200) ?
 								(
 									<div>
+										<i className="fas fa-times botonCerrarModal cursorManito" onClick={this.cerrarModal} />
+										<br />
 										<i className="fas fa-check-circle iconoModalOk" />
 										<br />
 										<br />
@@ -198,6 +225,8 @@ class CargarHistorico extends Component {
 									</div>
 								) : (
 									<div>
+										<i className="fas fa-times botonCerrarModal cursorManito" onClick={this.cerrarModalError} />
+										<br />
 										<i className="fas fa-exclamation-circle iconoModalError" />
 										<br />
 										<br />
@@ -209,9 +238,7 @@ class CargarHistorico extends Component {
 					</MDBModal>
 				}
 			</div >
-
 		)
 	}
-
 }
 export default CargarHistorico;
